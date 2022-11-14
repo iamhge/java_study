e7698a4bf05ebb7e4743e7cc39e8ba9797b33c5d
iamhge <fine632@gmail.com>
2022-11-15 01:12:59 +0900

    ArrayList 클래스(4)

---
+++ doitJava/Chapter7/src/arraylist/ArrayListTest.java
@@ -0,0 +1,16 @@
+package arraylist;
+
+import java.util.ArrayList;
+
+public class ArrayListTest {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("aaa");
+        list.add("bbb");
+        list.add("ccc");
+
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+    }
+}
